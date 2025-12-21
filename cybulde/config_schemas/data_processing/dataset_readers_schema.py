cf70f1cc1297815726493b500699b1f1e47744d3
torresv1 <torres_vladimir@hotmail.com>
2025-12-21 09:29:53 -0500

    Refactor dataset reader configuration: update target field naming and adjust YAML structure

--- cybulde/config_schemas/data_processing/dataset_readers_schema.py
+++ cybulde/config_schemas/data_processing/dataset_readers_schema.py
@@ -6,20 +6,20 @@ from pydantic.dataclasses import dataclass
 
 @dataclass
 class DatasetReaderConfig:
-    _target_: str = MISSING
+    target_: str = MISSING
     dataset_dir: str = MISSING
     dataset_name: str = MISSING
 
 
 @dataclass
 class GHCDatasetReaderConfig(DatasetReaderConfig):
-    _target_: str = "cybulde.data_processing.dataset_readers.GHCDatasetReader"
+    target_: str = "cybulde.data_processing.dataset_readers.GHCDatasetReader"
     dev_split_ratio: float = MISSING
 
 
 @dataclass
 class DatasetReaderManagerConfig:
-    _target_: str = "cybulde.data_processing.dataset_readers.DatasetReaderManager"
+    target_: str = "cybulde.data_processing.dataset_readers.DatasetReaderManager"
     dataset_readers: dict[str, DatasetReaderConfig] = MISSING
 
 
